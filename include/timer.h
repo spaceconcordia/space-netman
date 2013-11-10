19166cbf2edef34483669d57c7b911e83632b1ec
Gordon Bailey <gordon.p.bailey@gmail.com>
2013-11-10 16:37:10 -0500

    Added Olivier's timer.c and timer.h

--- include/timer.h
+++ include/timer.h
@@ -1,87 +1,22 @@
-#ifndef TIMER_H_
-#define TIMER_H_
-
+#include <stdio.h>
+#include <stdlib.h>
 #include <inttypes.h>
 #include <stdbool.h>
+#include <signal.h>
+#include <time.h>
 
-typedef /*
-
-whatever is neccessary for the implementation
-
-*/ timer_t;
-
-
-
-// This function should set the duration of `timer`
+// Create a new interval timer
+// Returns the ID of the new timer
 //
-// `timeout` is the amount of time until this timer finishes.
-// The units should be whatever makes most sense for us, I dunno
-// if that is milliseconds or seconds, but probably its one of those
-// two. If some type other than uint32_t makes more sense for this
-// then it should be changed
-void timer_set_timeout(timer_t * timer, uint32_t timeout);
+// We can choose to use a signal handler when the timer expires in the future
+// We would register to the signal handler by passing a function pointer to this function
+timer_t timer_get();
 
+// Arms or disarms the timer specified by timerid
+// The timer will expire at the given timeout value
+// Entering a timeout of 0 will disarm the timer
+// The function resets the time when it is called to the timeout value
+void timer_start(timer_t * timerid, uint32_t timeout);
 
-// This function starts `timer`. It should be assumed that `timer`
-// has already had its timeout value set with `timer_set_timeout`
-//
-// This function should reset the timer when it is called, and should be
-// able to be called multiple times in a row without causing problems
-void timer_start(timer_t * timer);
-
-// This function should return true if it has been more than `timeout`
-// units of time since `timer` was started, and false otherwise.
+// Returns true if the timer has expired
 bool timer_complete(timer_t * timer);
-
-/*
-
-Sample Usage:
-
-This is how the timer will be used in the NETMAN
-
-timer_t window_timer;
-timer_set_timeout(&window_timer, WINDOW_TIMEOUT);
-
-timer_t resend_timer;
-timer_set_timeout(&resend_timer, RESEND_TIMEOUT);
-
-while(session_active){
-
-
-   if(waiting_for_ack && timer_complete(&resend_timer)){
-      // We sent something, and have waited for a response but
-      // didn't get one. We will send the same thing again, because
-      // it probably got lost.
-      resend_data();
-      timer_start(&resend_timer);
-   }else if(new_data_to_send){
-      // We have new data to send, so we will send it
-      send_data();
-      timer_start(&resend_timer);
-   }
-
-   ...
-
-   if(successfully_received_something){
-      // Any time we receive good data, we should restart the window
-      // timer
-      timer_start(&window_timer);
-   }
-
-   ...
-
-   if(timer_complete(&window_timer)){
-      // If the window timer completes, it means we haven't received
-      // anything in a while, and the session should be ended.
-      close_session();
-      session_active = false;
-   }
-}
-
-...
-
-
-*/
-
-
-# endif
---
+++ src/timer.c
@@ -0,0 +1,60 @@
+timer_t timer_get()
+{
+	timer_t timer;
+    struct sigevent sev;
+
+    /*
+    struct sigaction sa;
+    // register to signal handler
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = signal_handler;
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(SIG, &sa, NULL) == -1)
+        errExit("Failed to register to signal handler");
+    */
+
+	// create timer
+    sev.sigev_notify = SIGEV_NONE;
+    sev.sigev_signo = SIGCONT;
+    sev.sigev_value.sival_ptr = &timer;
+
+	 if (timer_create(CLOCK_MONOTONIC, &sev, &timer) == -1) {
+		perror("Could not create timer\n");
+		exit(EXIT_FAILURE);
+	 }
+
+	 return timer;
+
+}
+
+void timer_start(timer_t * timer, uint32_t timeout)
+{
+	printf("timer starting\n");
+    struct itimerspec it_val;
+    it_val.it_value.tv_sec = timeout;
+    it_val.it_value.tv_nsec = 0;
+
+    // timer expires once
+    it_val.it_interval.tv_sec = 0;
+    it_val.it_interval.tv_nsec = 0;
+
+	 if (timer_settime(*timer, 0, &it_val, NULL) == -1) {
+			perror("Could not start timer\n");
+			exit(EXIT_FAILURE);
+	 }
+}
+
+
+bool timer_complete(timer_t * timer)
+{
+	struct itimerspec curr_val;
+	if(timer_gettime(*timer, &curr_val) == -1) {
+		perror("Could not create timer\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if(curr_val.it_value.tv_sec == 0)
+		return true;
+	else
+		return false;
+}
